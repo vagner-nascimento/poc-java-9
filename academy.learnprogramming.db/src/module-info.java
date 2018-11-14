8201046b2e00b63c16c6ddf35c18a0359b6122e9
Vagner Nascimento <vagnernasc88@gmail.com>
2018-11-13 22:22:19 -0200

    Building DB module

---
+++ academy.learnprogramming.db/src/module-info.java
@@ -0,0 +1,6 @@
+module academy.learnprogramming.db {
+    requires java.sql;
+// This is the name of the library(in Project Structure window - like sqllite-jdbc-3.21.0) without version
+    requires sqlite.jdbc;
+    requires academy.learnprogramming.common;
+}
