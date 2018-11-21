91defab716aace51ed252d9d002379046382e619
Vagner Nascimento <vagnernasc88@gmail.com>
2018-11-21 12:54:56 -0200

    Added ui and db module

--- academy.learnprogramming.db/src/module-info.java
+++ academy.learnprogramming.db/src/module-info.java
@@ -1,6 +1,8 @@
 module academy.learnprogramming.db {
     requires java.sql;
-// This is the name of the library(in Project Structure window - like sqllite-jdbc-3.21.0) without version
+//This is the name of the library (founded in Project Structure window - like sqllite-jdbc-3.21.0) without version
     requires sqlite.jdbc;
     requires academy.learnprogramming.common;
+
+    exports academy.learnprogramming.db;
 }
