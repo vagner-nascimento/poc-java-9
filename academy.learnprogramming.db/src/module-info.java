409acb50b3023f4d1e202979046fea8335e6ac28
Vagner Nascimento <vagnernasc88@gmail.com>
2018-11-22 13:37:55 -0200

    Initial studies completed

--- academy.learnprogramming.db/src/module-info.java
+++ academy.learnprogramming.db/src/module-info.java
@@ -1,8 +1,9 @@
 module academy.learnprogramming.db {
     requires java.sql;
 //This is the name of the library (founded in Project Structure window - like sqllite-jdbc-3.21.0) without version
-    requires sqlite.jdbc;
-    requires academy.learnprogramming.common;
+    requires sqlite.jdbc; //This is a AUTOMATIC module, a directly .jar file
+//transitive makes the required dependency accessible by modules that requires this module (db module)
+    requires transitive academy.learnprogramming.common;
 
     exports academy.learnprogramming.db;
 }
